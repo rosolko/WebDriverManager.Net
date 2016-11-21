029ea9a989351b6c96616bb6bd9f0df384645b4b
Alexander Rosolko <rosolko@gmail.com>
2016-11-21 21:30:15 +0300

    Initial intargation tests

---
+++ IntegrationTests/BinaryServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using WebDriverManager.Helpers;
+using WebDriverManager.Services.Impl;
+using Xunit;
+
+namespace IntegrationTests
+{
+    public class BinaryServiceTests : BinaryService
+    {
+        [Fact]
+        public void DownloadZipResultNotEmpty()
+        {
+            var url = "https://chromedriver.storage.googleapis.com/2.25/chromedriver_win32.zip";
+            var destination = FileHelper.GetZipDestination(url);
+            var result = DownloadZip(url, destination);
+            Assert.NotEmpty(result);
+            Assert.True(File.Exists(result));
+        }
+
+        [Fact]
+        public void UnZipResultNotEmpty()
+        {
+            var zipPath = Path.Combine(Directory.GetCurrentDirectory(), @"Assets\unzipable.zip");
+            var destination = FileHelper.GetBinDestination("Files", "2.0.0", Architecture.X32, "file.txt");
+            FileHelper.CreateDestinationDirectory(destination);
+            var result = UnZip(zipPath, destination, "file.txt");
+            Assert.NotEmpty(result);
+            Assert.True(File.Exists(result));
+        }
+
+        [Fact]
+        public void RemoveZipTargetMissing()
+        {
+            var zipPath = Path.Combine(Directory.GetCurrentDirectory(), @"Assets\removable.zip");
+            RemoveZip(zipPath);
+            Assert.False(File.Exists(zipPath));
+        }
+    }
+}
---
+++ IntegrationTests/VariableServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using WebDriverManager.Services.Impl;
+using Xunit;
+
+namespace IntegrationTests
+{
+    public class VariableServiceTests : VariableService
+    {
+        [Fact]
+        public void UpdatePathResultValid()
+        {
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), @"Assets\file.txt");
+            UpdatePath(filePath);
+            var pathVariable = Environment.GetEnvironmentVariable("PATH", EnvironmentVariableTarget.Process);
+            var variable = Path.GetDirectoryName(filePath);
+            Assert.NotNull(pathVariable);
+            Assert.Contains(variable, pathVariable);
+        }
+    }
+}

bcf2c99d2afda70fe41ed0b9ee6490940ace231a
Aliaksandr Rasolka <rosolko@gmail.com>
2018-04-09 15:00:56 +0300

    Improve test coverage.

---
+++ IntegrationTests/ServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+using WebDriverManager.Services.Impl;
+using Xunit;
+
+namespace IntegrationTests
+{
+    public class ServiceTests
+    {
+        private readonly BinaryService _customBinaryService;
+        private readonly VariableService _customVariableService;
+        private readonly ChromeConfig _chromeConfig;
+
+        public ServiceTests()
+        {
+            _customBinaryService = new BinaryService();
+            _customVariableService = new VariableService();
+            _chromeConfig = new ChromeConfig();
+        }
+
+        [Fact]
+        public void CustomServiceTest()
+        {
+            new DriverManager(_customBinaryService, _customVariableService).SetUpDriver(_chromeConfig);
+            var pathVariable = Environment.GetEnvironmentVariable("PATH", EnvironmentVariableTarget.Process);
+            Assert.NotNull(pathVariable);
+            Assert.Contains(_chromeConfig.GetName(), pathVariable);
+        }
+    }
+}

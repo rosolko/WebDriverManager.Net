b2495a10813643e3e5cb465a80735cbf91cc89c7
Aliaksandr Rasolka <rosolko@gmail.com>
2021-10-26 12:00:39 +0300

    Update OperaConfig.cs

--- WebDriverManager/DriverConfigs/Impl/OperaConfig.cs
+++ WebDriverManager/DriverConfigs/Impl/OperaConfig.cs
@@ -37,7 +37,7 @@ namespace WebDriverManager.DriverConfigs.Impl
                 var htmlCode = client.DownloadString("https://github.com/operasoftware/operachromiumdriver/releases");
                 var parser = new HtmlParser();
                 var document = parser.ParseDocument(htmlCode);
-                var version = document.QuerySelectorAll(".Link--primary")
+                var version = document.QuerySelectorAll("[class='Link--primary']")
                     .Select(element => element.TextContent)
                     .FirstOrDefault();
                 return version;
